733d2d5d1c40db9f05d30b0b7eb5912850fcfd16
eduardo bauer <eduardofbauer@gmail.com>
2025-03-10 14:18:14 -0300

    java basico

---
+++ javaBasico/src/edu/java/anatomiadasclasses/MinhaClasse.java
@@ -0,0 +1,34 @@
+package edu.java.anatomiadasclasses;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        // printando na saida do sistema "teste de print".
+        System.out.println("teste de print");
+
+        // Variavel com o primeiro nome minusculo por ser modificada.
+        String primeiroNome = "Eduardo";
+        String segundoNome = "Bauer";
+        String nomeCompleto = juncaoNomes(primeiroNome, segundoNome);
+        int anoDeNascimento = 2021;
+        double valorTotal;
+        boolean simNao = true;
+
+        // Variabel toda maiuscula é uma variavel constante.
+        final String BR = "Brasil";
+        double PI = 3.14;
+        int ESTADOS_BRASILEIROS = 27;
+
+        // Manipulacao das variaveis.
+        anoDeNascimento = 2025;
+        System.out.println(nomeCompleto);
+    }
+
+    // Metodos TipoRetorno NomeObjetoNoInfinitivo Parametro(s)
+    public static int somar (int numero1, int numero2){
+        return numero1 + numero2;
+    }
+
+    public static String juncaoNomes(String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
---
+++ javaBasico/src/edu/java/tiposevariaveis/TiposEVariaveis.java
@@ -0,0 +1,4 @@
+package edu.java.tiposevariaveis;
+
+public class TiposEVariaveis {
+}
